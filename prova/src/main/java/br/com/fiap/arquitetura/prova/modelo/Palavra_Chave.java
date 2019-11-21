912d6f330a7d10d81e890878d153f6b87d2f6da4
Ppss1989 <patriciasilva_rj@yahoo.com.br>
2019-11-20 21:43:12 -0300

    Update Palavra_Chave.java

--- prova/src/main/java/br/com/fiap/arquitetura/prova/modelo/Palavra_Chave.java
+++ prova/src/main/java/br/com/fiap/arquitetura/prova/modelo/Palavra_Chave.java
@@ -1,5 +1,51 @@
-package br.com.fiap.arquitetura.prova.modelo;
-
-public class Palavra_Chave {
+ @Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	@NotBlank@NotNull
+	private String nome;
+	@OneToOne(mappedBy = "filme")
+	private List<Filme> filme;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Palavra_Chave other = (Palavra_Chave) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public List<Filme> getFilme() {
+		return filme;
+	}
+	public void setFilme(List<Filme> filme) {
+		this.filme = filme;
+	}
+	 
 
 }
