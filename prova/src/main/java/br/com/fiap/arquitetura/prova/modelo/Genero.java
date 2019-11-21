23e74da354e128210b83107149fbcd3895c9a3ca
Ppss1989 <patriciasilva_rj@yahoo.com.br>
2019-11-20 21:16:54 -0300

    Update Genero.java

--- prova/src/main/java/br/com/fiap/arquitetura/prova/modelo/Genero.java
+++ prova/src/main/java/br/com/fiap/arquitetura/prova/modelo/Genero.java
@@ -1,5 +1,64 @@
 package br.com.fiap.arquitetura.prova.modelo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
 public class Genero {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	@NotBlank@NotNull
+	private String nome;
+	@OneToMany(mappedBy = "filme")
+	private Filme filme;
+	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Genero other = (Genero) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Filme getFilme() {
+		return filme;
+	}
+	public void setFilme(Filme filme) {
+		this.filme = filme;
+	}
 
 }
